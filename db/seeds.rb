0e38100d421d6a644b4a5833bb088daf2271f62d
Glen Horton <hortongn@ucmail.uc.edu>
2020-09-26 16:51:20 -0400

    Add seeds for Code of Conduct Types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CodeOfConductType.all.destroy_all
+CodeOfConductType.create(id: 1, name: 'Citizen', url: 'http://citizencodeofconduct.org')
+CodeOfConductType.create(id: 2, name: 'Contributor Covenant', url: 'https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.txt')
+CodeOfConductType.create(id: 3, name: 'Django', url: 'https://www.djangoproject.com/conduct/')
+CodeOfConductType.create(id: 4, name: 'Geek Feminism', url: 'https://geekfeminismdotorg.wordpress.com/about/code-of-conduct/')
+CodeOfConductType.create(id: 5, name: 'Open Code of Conduct', url: 'https://github.com/todogroup/opencodeofconduct')
+CodeOfConductType.create(id: 6, name: 'Python', url: 'https://www.python.org/psf/conduct/')
+CodeOfConductType.create(id: 7, name: 'Ubuntu', url: 'https://ubuntu.com/community/code-of-conduct')
