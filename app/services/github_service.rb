44556891950dd18dff1961e0d722fbec470f7709
Glen Horton <hortongn@ucmail.uc.edu>
2020-09-30 22:13:54 -0400

    Enhance GitHub service

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,25 +1,20 @@
 class GithubService
 
-  def search_repository(query, repository)
-    @github = Octokit::Client.new
-    results = @github.search_code "#{query} in:file repo:#{repository.login}/#{repository.name}"
+  def search_repository(repository)
+    CodeOfConductType.all.each do |code_of_conduct_type|
+      @github = Octokit::Client.new(:netrc => true)
+      results = @github.search_code "#{code_of_conduct_type.fingerprint_1} in:file repo:#{repository.login}/#{repository.name}"
 
-    code_of_conduct_paths = []
-
-    results.items.each do |item|
-      code_of_conduct_paths << item.path
+      results.items.each do |item|
+        CodeOfConduct.create(path: item.path, repository_id: repository.id, code_of_conduct_type_id: code_of_conduct_type.id)
+      end
+      sleep 20
     end
-
-    return code_of_conduct_paths
   end
 
   def get_codes_of_conduct(query, repository)
     search_repository(query, repository).each do |path|
       CodeOfConduct.create(path: path, repository_id: repository.id, code_of_conduct_type_id: identify_code_of_conduct_type)
     end
-  end
-
-  def identify_code_of_conduct_type
-    return 2
   end
 end
