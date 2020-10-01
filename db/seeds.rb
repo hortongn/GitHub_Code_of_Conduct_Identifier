b20e4fc745a42e994de892c8ae546084af198d24
Glen Horton <hortongn@ucmail.uc.edu>
2020-09-30 22:12:59 -0400

    Tweak seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ CodeOfConductType.all.destroy_all
 CodeOfConductType.create(id: 1, name: 'Contributor Covenant', url: 'https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.txt', fingerprint_1: 'adapted from the Contributor Covenant')
 CodeOfConductType.create(id: 2, name: 'Django', url: 'https://www.djangoproject.com/conduct/', fingerprint_1: 'Be careful in the words that you choose')
 CodeOfConductType.create(id: 3, name: 'Geek Feminism', url: 'https://geekfeminismdotorg.wordpress.com/about/code-of-conduct/', fingerprint_1: 'We do not tolerate harassment of participants in any form')
-CodeOfConductType.create(id: 4, name: 'Open Code of Conduct', url: 'https://github.com/todogroup/opencodeofconduct', fingerprint_1: 'Our goal is to make explicit what we expect from participants in this community as well as its leaders')
+CodeOfConductType.create(id: 4, name: 'Open Code of Conduct', url: 'https://github.com/todogroup/opencodeofconduct', fingerprint_1: 'Our goal is to make explicit what we expect from participants in this community')
 CodeOfConductType.create(id: 5, name: 'Python', url: 'https://www.python.org/psf/conduct/', fingerprint_1: 'Members of the community are considerate of their peers')
 CodeOfConductType.create(id: 6, name: 'Ubuntu', url: 'https://ubuntu.com/community/code-of-conduct', fingerprint_1: 'Disagreement is no excuse for poor manners')
+CodeOfConductType.create(id: 7, name: 'Generic', url: nil, fingerprint_1: 'code of conduct')
