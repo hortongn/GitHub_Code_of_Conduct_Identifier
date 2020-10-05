648e24e0fb9b5c6bb97789e80eb742b5c9d40883
Glen Horton <hortongn@ucmail.uc.edu>
2020-10-05 18:57:48 -0400

    Tweak GitHubService

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,13 +3,19 @@ class GithubService
   def search_repository(repository)
     CodeOfConductType.all.each do |code_of_conduct_type|
       @github = Octokit::Client.new(:netrc => true)
-      results = @github.search_code "#{code_of_conduct_type.fingerprint_1} in:file repo:#{repository.login}/#{repository.name}"
+      results = @github.search_code "\"#{code_of_conduct_type.fingerprint_1}\" in:file repo:#{repository.login}/#{repository.name}"
 
       results.items.each do |item|
-        CodeOfConduct.create(path: item.path, repository_id: repository.id, code_of_conduct_type_id: code_of_conduct_type.id)
+        unless file_is_previously_found?(item, repository)
+          CodeOfConduct.create(path: item.path, url: item.html_url, repository_id: repository.id, code_of_conduct_type_id: code_of_conduct_type.id)
+        end
       end
-      sleep 20
+      sleep 60
     end
+  end
+
+  def file_is_previously_found?(item, repository)
+    return true unless CodeOfConduct.where(path: item.path, repository_id: repository.id).blank?
   end
 
   def get_codes_of_conduct(query, repository)
