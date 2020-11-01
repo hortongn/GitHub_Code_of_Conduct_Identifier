a337ad906d5fdd41edbe856abe280bbe0b2683f2
Glen Horton <hortongn@ucmail.uc.edu>
2020-10-31 20:14:03 -0400

    Remove unneeded repositories views

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -6,37 +6,12 @@ class RepositoriesControllerTest < ActionDispatch::IntegrationTest
     @repository = repositories(:one)
   end
 
-  test "should get index" do
-    get repositories_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_repository_url
-    assert_response :success
-  end
-
   test "should create repository" do
     assert_difference('Repository.count') do
       post repositories_url, params: { repository: { description: @repository.description, login: @repository.login, name: @repository.name, stars: @repository.stars, url: @repository.url, repository_set_id: @repository_set.id } }
     end
 
     assert_redirected_to repository_url(Repository.last)
-  end
-
-  test "should show repository" do
-    get repository_url(@repository)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_repository_url(@repository)
-    assert_response :success
-  end
-
-  test "should update repository" do
-    patch repository_url(@repository), params: { repository: { description: @repository.description, login: @repository.login, name: @repository.name, stars: @repository.stars, url: @repository.url } }
-    # assert_redirected_to repository_url(@repository)
   end
 
   test "should destroy repository" do
